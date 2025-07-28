b2d8d660448f5ead5018dd91d54f3a73c807c669
Marco Mazzai <mazzai.marco@gmail.com>
2025-07-28 16:39:26 +0200

    feat: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,9 +4,9 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     '@storybook/addon-a11y',
+    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/web-components-vite",
